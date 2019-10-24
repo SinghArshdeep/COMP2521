248aff29de1ccf9489daa2e08b70dcbd626af443
Arsh <arshdeep.bhogal@yahoo.com>
2019-10-24 20:01:25 +1100

    Update TreeSumOdds.c

--- pr/TreeSumOdds/TreeSumOdds.c
+++ pr/TreeSumOdds/TreeSumOdds.c
@@ -7,15 +7,13 @@ int TreeSumOdds(Tree t) {
 	if (t == NULL)
 	{
 		return 0;
-	}else if (t->value % 2 != 0)
-	{
-		return t->value;
-	}else
-	{
-		return 0;
 	}
 	int left = TreeSumOdds(t->left); 
 	int right = TreeSumOdds(t->right);
-	return left + right;
+	if (t->value % 2 == 1)
+	{
+		return t->value + left + right;
+	}
+	return 0;
 }
 
