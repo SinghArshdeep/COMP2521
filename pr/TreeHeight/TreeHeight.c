ac190bf8c3e743ee4ccc105a939fa7ef4d6f1fb2
Arsh <arshdeep.bhogal@yahoo.com>
2019-10-24 19:17:52 +1100

    Update TreeHeight.c

--- pr/TreeHeight/TreeHeight.c
+++ pr/TreeHeight/TreeHeight.c
@@ -4,6 +4,19 @@
 #include "tree.h"
 
 int TreeHeight(Tree t) {
-    return -1;
+    if (t == NULL)
+    {
+        return -1;
+    }
+    int left = TreeHeight(t->left);
+    int right = TreeHeight(t->right);
+    if (left >= right)
+    {
+        return 1 + left;
+    }else
+    {
+        return 1+ right;
+    }
+    
 }
 
