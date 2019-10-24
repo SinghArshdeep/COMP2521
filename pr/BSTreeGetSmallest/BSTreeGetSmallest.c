c10f91b3c94dd0f55c9cc97749f8894c72072d74
Arsh <arshdeep.bhogal@yahoo.com>
2019-10-24 19:11:14 +1100

    a

---
+++ pr/BSTreeGetSmallest/BSTreeGetSmallest.c
@@ -0,0 +1,21 @@
+
+#include "BSTree.h"
+
+#include <stdlib.h>
+
+BSTree BSTreeGetSmallest(BSTree t) {
+	if (t == NULL)
+	{
+		return NULL;
+	}else if (t->left == NULL)
+	{
+		return t;
+	}else
+	{
+		return BSTreeGetSmallest(t);
+	}
+	
+	
+	
+}
+
